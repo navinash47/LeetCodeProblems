896b4e8ae63e7e9acd4675504bde567e5e91b1a2
Avinash Nandyala <42831255+navinash47@users.noreply.github.com>
2024-08-04 22:17:11 +0000

    Reversearray added  only recursive

--- takeuforward/factorial_rec.cpp
+++ takeuforward/factorial_rec.cpp
@@ -1,38 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include "Timer.h"
 
 using namespace std;
 
-int factorial(int n){
-    if(n==1 || n==0){
-        return 1;
-    }
-    return n* factorial(n-1);
+int factorial(int n)
+{
+  if (n == 1 || n == 0)
+  {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
-int factorial_iterative(int n){
-  if(n==0){
+int factorial_iterative(int n)
+{
+  if (n == 0)
+  {
     return 1;
   }
   stack<int> s;
-  while(n!=0){
+  while (n != 0)
+  {
     s.push(n);
     n--;
   }
-  int res=1;
-  while(!s.empty()){
-    res=res*s.top();
+  int res = 1;
+  while (!s.empty())
+  {
+    res = res * s.top();
     s.pop();
   }
   return res;
 }
 
-int main(){
-  
-  // Here, let’s take the value of n to be 3.
-  int n = 5; 
-  cout << "Result of factorial: " << factorial(n)<<endl;
-  cout << "Result of factorial (iterative): " << factorial_iterative(n)<<endl;
-  return 0;
+int main()
+{
 
+  // Here, let’s take the value of n to be 3.
+  int n = 5;
+  cout << "Result of factorial: " << factorial(n) << endl;
+  cout << "Result of factorial (iterative): " << factorial_iterative(n) << endl;
+  return 0;
 }
